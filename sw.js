db1ad5719661dce81e62d807c1ab6fd429ebf035
ALEX SANTOS LIMA <alex.slima13@senacsp.edu.br>
2025-02-14 20:06:07 -0300

    carta

--- sw.js
+++ sw.js
@@ -1,35 +1,32 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches.open('static')
-      .then ((cache) => {
-          cache.add('./Carta/index.html')
-           cache.add('./Carta/style.css')
-           cache.add('./Carta/app.js')
-           cache.add('./Carta/img/valete.png')
-           cache.add('./Carta/img/dama.png')
-           cache.add('./Carta/img/rei.png')
-           cache.add('./Carta/img/icon-192x192.png')
-           cache.add('./Carta/img/icon-512x512.png')
-           ;
-      })
+        caches.open('static')
+            .then((cache) => {
+                return cache.addAll([
+                    './Carta/index.html',
+                    './Carta/style.css',
+                    './Carta/app.js',
+                    './Carta/img/valete.png',
+                    './Carta/img/dama.png',
+                    './Carta/img/rei.png',
+                    './Carta/img/icon-192x192.png',
+                    './Carta/img/icon-512x512.png'
+                ]);
+            })
     );
-  });
- 
-  self.addEventListener('activate',(event) => {
-    console.log("ativando o serviço worker...",event)
-    return self.clients.claim()
-})
- 
-//interceptação (solicitação https servindo em cache quando off-line)
+});
+
+self.addEventListener('activate', (event) => {
+    console.log("Ativando o service worker...", event);
+    return self.clients.claim();
+});
+
+// Interceptação de solicitações para servir do cache quando offline
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-        .then((response) => {
-            if(response) {
-                return response
-            } else {
-                return fetch(event.request)
-            }
-        })
-    )
-})
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
